8035751b62a6492bff556108e4832106170893c4
Bill Walker <bwalker@mozilla.com>
2015-05-28 20:41:09 -0700

    start using browserify to pull in the lectionary module for now, use my json files for the lectionary (to be moved) get rid of hour/minute/day hands try rollover

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 // multiclock app.js
 
+// show date each sunday
+// put advent 1 at the top always
+// have marker slowly move clockwise. you know, like a clock
+// advent is not red, blue!
+// easter is white!
+
+var lectionary = require('lectionary');
+
+var yearBScriptures = require('./year-b.json')
+var yearAScriptures = require('./year-a.json')
+var yearCScriptures = require('./year-c.json')
+
+function getScriptures(lectionaryDate) {
+	if (lectionaryDate.lectionaryYear == 'A' || lectionaryDate.lectionaryYear == 'A-B-C') {			
+		var scriptures = yearAScriptures[lectionaryDate.lectionaryShortName];
+	} else if (lectionaryDate.lectionaryYear == 'B') {			
+		var scriptures = yearBScriptures[lectionaryDate.lectionaryShortName];
+	} else if (lectionaryDate.lectionaryYear == 'C') {			
+		var scriptures = yearCScriptures[lectionaryDate.lectionaryShortName];
+	} else {
+		throw 'unknown lectionary year ' + lectionaryDate.lectionaryYear;
+	}
+
+	if (! scriptures) {
+		throw 'missing scriptures ' + JSON.stringify(lectionaryDate) + ' ' + year;
+	}
+
+	return scriptures;
+}
+
 var beginYear = new Date();
 beginYear.setMonth(0);
 beginYear.setDate(1);
@@ -20,58 +50,6 @@ var dayScale = d3.scale.linear().domain([24, 0]).range([0, 2 * Math.PI]);
 var yearScale = d3.scale.linear().domain([endYear.getTime(), beginYear.getTime()]).range([0, 2 * Math.PI]);
 var yearDegreesScale = d3.scale.linear().domain([beginYear.getTime(), endYear.getTime()]).range([0, 360]);
 
-function initializeDayCircle(face) {
-	// set of arcs representing 24hours, showing sunlight
-
-	var dayCircle = face.append('g')
-		.attr('id','dayCircle');
-
-	var sunlightArc = d3.svg.arc()
-		.innerRadius(200)
-		.outerRadius(220)
-		.startAngle(dayScale(6))
-		.endAngle(dayScale(20));
-
-	dayCircle.append("path")
-		.attr("id", 'sunlightArc')
-		.attr("d", sunlightArc)
-
-	var nightArc = d3.svg.arc()
-		.innerRadius(200)
-		.outerRadius(220)
-		.startAngle(dayScale(20))
-		.endAngle(dayScale(30));
-
-	dayCircle.append("path")
-		.attr("id", 'nightArc')
-		.attr("d", nightArc);
-}
-
-function initializeClockArcs(face) {
-	// small mark that weeps once a minute
-
-	var minuteArc = d3.svg.arc()
-		.innerRadius(80)
-		.outerRadius(85)
-		.startAngle(-0.025 * Math.PI)
-		.endAngle(0.025 * Math.PI);
-
-	face.append("path")
-		.attr("id", 'minuteArc')
-		.attr("d", minuteArc)
-
-	// small arc that sweeps once an hour
-
-	var hourArc = d3.svg.arc()
-		.innerRadius(87)
-		.outerRadius(92)
-		.startAngle(-0.025 * Math.PI)
-		.endAngle(0.025 * Math.PI);
-
-	face.append("path")
-		.attr("id", 'hourArc')
-		.attr("d", hourArc);	
-}
 
 function getEasterForYear(Y) {
     // http://coderzone.org/library/Get-Easter-Date-for-any-year-in-Javascript_1059.htm
@@ -119,33 +97,38 @@ function getSeasonsForYear(Y) {
     // console.log(days);
 
     var seasons = [
-	    [days.advent.getTime(), days.christmas.getTime(), 'advent'],
-	    [days.christmas.getTime(), days.nye.getTime(), 'epiphany'],
-	    [days.nyd.getTime(), days.epiphany.getTime(), 'epiphany'],
-	    [days.epiphany.getTime(), days.ashWednesday.getTime(), 'ordinary'],
-	    [days.ashWednesday.getTime(), days.palmSunday.getTime(), 'lent'],
-	    [days.palmSunday.getTime(), days.easter.getTime(), 'holyWeek'],
-	    [days.easter.getTime(), days.pentecost.getTime(), 'easter'],
-	    [days.pentecost.getTime(), days.advent.getTime(), 'ordinary']
+	    [days.advent.getTime(), days.christmas.getTime(), 'advent', 'Advent'],
+	    [days.christmas.getTime(), days.nye.getTime(), 'epiphany', 'Epiphany'],
+	    [days.nyd.getTime(), days.epiphany.getTime(), 'epiphany', 'Epiphany'],
+	    [days.epiphany.getTime(), days.ashWednesday.getTime(), 'ordinary', 'Ordinary Time'],
+	    [days.ashWednesday.getTime(), days.palmSunday.getTime(), 'lent', 'Lent'],
+	    [days.palmSunday.getTime(), days.easter.getTime(), 'holyWeek', 'Holy Week'],
+	    [days.easter.getTime(), days.pentecost.getTime(), 'easter', 'Season of Easter'],
+	    [days.pentecost.getTime(), days.advent.getTime(), 'ordinary', 'Ordinary Time']
     ];
 
     // console.log(seasons);
     return seasons;
 }
 
-function addSundays(seasons, Y) {
-	var D = new Date(Y,0,1)
-	var day = D.getDay();
-	if (day != 0) D.setDate(D.getDate() + (7 - day));
+function getSundays(year) {
+	console.log('addSundays', year);
+	var sundays = [];
 
-	while (D) {
-	    D.setDate(D.getDate()+7);
-	    if (D.getFullYear() != Y) return;
-	    seasons.push([D.getTime(), D.getTime() + oneDay, 'sunday']);
+	for (var month = 0; month < 12; month++) {
+		var lectionaryDates = lectionary(year, month);
+		for (var index in lectionaryDates) {
+			var aSunday = lectionaryDates[index];
+			if (aSunday.date.getDay() == 0) {
+				sundays.push([aSunday.date.getTime(), aSunday.date.getTime() + 6.9*oneDay, aSunday.lectionaryShortName, JSON.stringify(getScriptures(aSunday))]);
+			}
+		}
 	}
+	
+	return sundays;
 }
 
-function initializeYearCircle(face) {
+function initializeSeasonCircle(face) {
 	// set of arcs representing a year, showing liturgical calendar
 
 	var yearCircle = face.append('g')
@@ -153,10 +136,10 @@ function initializeYearCircle(face) {
 
 	var seasons = getSeasonsForYear(new Date().getFullYear());
 
-	addSundays(seasons, new Date().getFullYear());
+	getSundays(new Date().getFullYear());
 
 	var seasonArc = d3.svg.arc()
-		.innerRadius(300)
+		.innerRadius(230)
 		.outerRadius(340)
 		.startAngle(function(d) {
 			return yearScale(d[0]);
@@ -173,12 +156,68 @@ function initializeYearCircle(face) {
 		.attr("id", function(d){
 			return d[2];
 		})
+		.attr("data-scriptures", function(d){
+			return d[3];
+		})
 		.on('mouseover', function(d) {
-			console.log(d[2], new Date(d[0]).toLocaleDateString(), new Date(d[1]).toLocaleDateString());
+			document.getElementById('selectionName').textContent = d[3];
+			document.getElementById('first').textContent = new Date(d[0]).toLocaleDateString();
+			document.getElementById('second').textContent = new Date(d[1]).toLocaleDateString();
+		})
+		.on('mouseleave', function(d) {
+			document.getElementById('selectionName').textContent = '';
+			document.getElementById('first').textContent = '';
+			document.getElementById('second').textContent = '';
 		});
+;
 
 	yearCircle.selectAll("path").data(seasons).enter();
 }
+
+function initializeWeekCircle(face) {
+	// set of arcs representing a year, showing weekly calendar
+
+	var weekCircle = face.append('g')
+		.attr('id', 'weekCircle');
+
+	var sundays = getSundays(new Date().getFullYear());
+
+	var weekArc = d3.svg.arc()
+		.innerRadius(250)
+		.outerRadius(300)
+		.startAngle(function(d) {
+			return yearScale(d[0]);
+		})
+		.endAngle(function(d){ 
+			return yearScale(d[1]);
+		});
+
+	weekCircle.selectAll("path")
+		.data(sundays)
+		.enter()
+		.append("path")
+		.attr("d", weekArc)
+		.attr('class', 'sunday')
+		.on('mouseover', function(d) {
+			document.getElementById('selectionName').textContent = d[2];
+			scriptures = JSON.parse(d[3]);
+			if (scriptures.complementary) { scriptures = scriptures.complementary }
+			document.getElementById('first').textContent = scriptures.first;
+			document.getElementById('second').textContent = scriptures.second;
+			document.getElementById('psalm').textContent = scriptures.psalm;
+			document.getElementById('gospel').textContent = scriptures.gospel;
+		})
+		.on('mouseleave', function(d) {
+			document.getElementById('selectionName').textContent = '';
+			document.getElementById('first').textContent = '';
+			document.getElementById('second').textContent = '';
+			document.getElementById('psalm').textContent = '';
+			document.getElementById('gospel').textContent = '';
+		});
+
+	weekCircle.selectAll("path").data(sundays).enter();
+}
+
 
 function initializeClock() {
 	var vis = d3.select("#svg_donut");
@@ -190,7 +229,7 @@ function initializeClock() {
 	// background gray circle
 
 	var fullArc = d3.svg.arc()
-		.innerRadius(80)
+		.innerRadius(150)
 		.outerRadius(340)
 		.startAngle(0)
 		.endAngle(2 * Math.PI);
@@ -199,11 +238,9 @@ function initializeClock() {
 		.attr("id", 'fullArc')
 		.attr("d", fullArc)
 
-	initializeClockArcs(face);
+	initializeSeasonCircle(face);
 
-	initializeDayCircle(face);
-
-	initializeYearCircle(face);
+	initializeWeekCircle(face);
 
 	// vertical mark showing now
 
@@ -212,33 +249,12 @@ function initializeClock() {
       .attr("x", 0)
       .attr("width", 1)
       .attr("y", -340)
-      .attr("height", 260);			
+      .attr("height", 190);			
 }
 
 function step(timestamp) {
 
-	d3.select('#minuteArc')
-		.transition()
-		.attr('transform', function(d){
-			var angle = 360 * ((new Date().getTime() / 1000) % 60) / 60.0;
-			return 'rotate(' + Math.round(angle) + ')';
-		});
-
-	d3.select('#hourArc')
-		.transition()
-		.attr('transform', function(d){
-			var angle = 360 * ((new Date().getTime() / 1000) % 3600) / 3600.0;
-			return 'rotate(' + angle + ')';
-		});
-
-	d3.select('#dayCircle')
-		.transition()
-		.attr('transform', function(d){
-			var tmp = new Date();
-			var percentage = (tmp.getHours() * 3600 + tmp.getMinutes() * 60) / 86400.0;
-			var angle = 360 * percentage;
-			return 'rotate(' + angle + ')';
-		});
+	document.getElementById('timeView').textContent = new Date().getFullYear();
 
 	d3.select('#yearCircle')
 		.transition()
@@ -247,8 +263,15 @@ function step(timestamp) {
 			return 'rotate(' + yearDegreesScale(tmp.getTime()) + ')';
 		});
 
-	window.setTimeout(step, 1000);
+	d3.select('#weekCircle')
+		.transition()
+		.attr('transform', function(d){
+			var tmp = new Date();
+			return 'rotate(' + yearDegreesScale(tmp.getTime()) + ')';
+		});
+
+	window.setTimeout(step, 10000);
 }
 
 initializeClock();
-window.setTimeout(step, 1000);
+window.setTimeout(step, 10000);
