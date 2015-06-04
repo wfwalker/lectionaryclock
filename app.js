8fb2846b96e19ae417bb56864e12224db396fbc6
Bill Walker <bwalker@mozilla.com>
2015-06-04 10:02:23 -0700

    add parsing of year from URL fragment (incomplete) use seasons from lectionary npm module remove redundant code for same

--- app.js
+++ app.js
@@ -4,80 +4,24 @@
 
 var lectionary = require('lectionary');
 
+var oneDay = 1000 * 60 * 60 * 24;
+
 var currentSunday = null;
 
 var currentYear = new Date().getFullYear();
+var tmp = window.location.toString();
+if (tmp.indexOf('#') > 0) {
+	currentYear = tmp.substring(tmp.indexOf('#')+1, tmp.indexOf('#')+5)
+}
+
 var endYear = new Date(currentYear, 11, 31, 23, 59, 59);
 var beginYear = new Date(currentYear, 0, 1, 0, 0, 0);
-
-var oneDay = 1000 * 60 * 60 * 24;
-
-var dayScale = d3.scale.linear().domain([24, 0]).range([0, 2 * Math.PI]);
-var yearScale = d3.scale.linear().domain([beginYear.getTime(), endYear.getTime()]).range([0, 2 * Math.PI]);
 var yearDegreesScale = d3.scale.linear().domain([beginYear.getTime(), endYear.getTime()]).range([0, 360]);
-
-function getEasterForYear(Y) {
-    // http://coderzone.org/library/Get-Easter-Date-for-any-year-in-Javascript_1059.htm
-    var C = Math.floor(Y/100);
-    var N = Y - 19*Math.floor(Y/19);
-    var K = Math.floor((C - 17)/25);
-    var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;
-    I = I - 30*Math.floor((I/30));
-    I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));
-    var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);
-    J = J - 7*Math.floor(J/7);
-    var L = I - J;
-    var M = 3 + Math.floor((L + 40)/44);
-    var D = L + 28 - 31*Math.floor(M/4);
- 
-    return new Date(Y, M-1, D);
-}
-
-function getSeasonsForYear(Y) {
-    var days = {};
-
-    // epiphany is fixed
-    days.epiphany = new Date(Y,0,6);
-    // easter follows crazy formula above
-    days.easter = getEasterForYear(Y);
-    // ash wednesday fixed days before easter
-    days.ashWednesday = new Date(days.easter.getTime() - 45 * oneDay);
-    // palm sunday one week before easter
-    days.palmSunday = new Date(days.easter.getTime() - 7 * oneDay);
-    // pentecost 50 days after easter
-    days.pentecost = new Date(days.easter.getTime() + 49 * oneDay);
-    // ascension 40 days after easter
-    days.ascension = new Date(days.easter.getTime() + 39 * oneDay);
-
-    days.nye = new Date(Y, 11, 31, 23, 59, 59);
-    days.nyd = new Date(Y, 0, 1, 0, 0, 0);
-
-    // christmas is fixed
-    days.christmas = new Date(Y,11,25);
-
-    //advent starts the closest sunday before nov 30?
-    days.advent = new Date(Y, 10, 30);
-    days.advent = new Date(days.advent.getTime() - days.advent.getDay() * oneDay);
-
-    // console.log(days);
-
-    var seasons = [
-	    [days.advent, days.christmas, 'advent', 'Advent'],
-	    [days.christmas, days.nye, 'epiphany', 'Epiphany'],
-	    [days.nyd, days.epiphany, 'epiphany', 'Epiphany'],
-	    [days.epiphany, days.ashWednesday, 'ordinary', 'Ordinary Time'],
-	    [days.ashWednesday, days.palmSunday, 'lent', 'Lent'],
-	    [days.palmSunday, days.easter, 'holyWeek', 'Holy Week'],
-	    [days.easter, days.pentecost, 'easter', 'Season of Easter'],
-	    [days.pentecost, days.advent, 'ordinary', 'Ordinary Time']
-    ];
-
-    // console.log(seasons);
-    return seasons;
-}
 
 function getSundays(year) {
 	var sundays = [];
+
+	// TODO: this does nothing except find the next sunday.
 
 	var lectionaryDates = lectionary.days(year);
 	for (var index in lectionaryDates) {
@@ -95,24 +39,24 @@ function getSundays(year) {
 }
 
 function initializeSeasonCircle(face) {
+	var yearScale = d3.scale.linear().domain([beginYear.getTime(), endYear.getTime()]).range([0, 2 * Math.PI]);
+	
 	// set of arcs representing a year, showing liturgical calendar
 
 	var yearCircle = face.append('g')
 		.attr('id', 'yearCircle');
 
-	var seasons = getSeasonsForYear(new Date().getFullYear());
-
-	getSundays(new Date().getFullYear());
+	var seasons = lectionary.seasons(currentYear);
 
 	var seasonArc = d3.svg.arc()
 		.innerRadius(250)
 		.outerRadius(375)
 		.cornerRadius(10)
 		.startAngle(function(d) {
-			return yearScale(d[0].getTime());
+			return yearScale(d.start.getTime());
 		})
 		.endAngle(function(d){ 
-			return yearScale(d[1].getTime());
+			return yearScale(d.end.getTime());
 		});
 
 	yearCircle.selectAll("path")
@@ -122,10 +66,10 @@ function initializeSeasonCircle(face) {
 		.attr("d", seasonArc)
 		.attr('stroke-width', 2)
 		.attr("class", function(d){
-			return 'season ' + d[2];
+			return 'season ' + d.name.replace(' ', '').toLowerCase();
 		})
 		.append('svg:title')
-		.text(function(d) { return d[3] + ', ' + d[0].toLocaleDateString() + ' - ' + d[1].toLocaleDateString(); });
+		.text(function(d) { return d.name + ', ' + d.start.toLocaleDateString() + ' - ' + d.end.toLocaleDateString(); });
 
 	yearCircle.selectAll("path").data(seasons).enter();
 }
@@ -159,7 +103,7 @@ function initializeWeekCircle(face) {
 	var weekCircle = face.append('g')
 		.attr('id', 'weekCircle');
 
-	var sundays = getSundays(new Date().getFullYear());
+	var sundays = getSundays(currentYear);
 
 	// Add a text label.
 	weekCircle.selectAll("text")
@@ -194,9 +138,9 @@ var face = vis.append('g')
 	.attr('id','clock-face')
 	.attr('transform','translate(375,375)');	
 
-// vertical mark showing now
-
 initializeSeasonCircle(face);
+
+// radial mark showing now
 
 var pointerArc = d3.svg.arc()
 	.innerRadius(255)
@@ -212,7 +156,7 @@ face.append("path")
 
 initializeWeekCircle(face);
 
-document.getElementById('timeView').textContent = new Date().getFullYear();
+document.getElementById('timeView').textContent = currentYear;
 
 d3.select('#pointer')
 	.transition().duration(1000)
