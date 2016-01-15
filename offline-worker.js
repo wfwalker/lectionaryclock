2f89767c60783e835048e6ae6bb51a5236e17bed
Travis CI <bwalker@mozilla.com>
2016-01-15 22:26:54 +0000

    Updates

---
+++ offline-worker.js
@@ -0,0 +1,148 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+
+(function (self) {
+  'use strict';
+
+  // On install, cache resources and skip waiting so the worker won't
+  // wait for clients to be closed before becoming active.
+  self.addEventListener('install', function (event) {
+    event.waitUntil(oghliner.cacheResources().then(function () {
+      return self.skipWaiting();
+    }));
+  });
+
+  // On activation, delete old caches and start controlling the clients
+  // without waiting for them to reload.
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(oghliner.clearOtherCaches().then(function () {
+      return self.clients.claim();
+    }));
+  });
+
+  // Retrieves the request following oghliner strategy.
+  self.addEventListener('fetch', function (event) {
+    if (event.request.method === 'GET') {
+      event.respondWith(oghliner.get(event.request));
+    } else {
+      event.respondWith(self.fetch(event.request));
+    }
+  });
+
+  var oghliner = self.oghliner = {
+
+    // This is the unique prefix for all the caches controlled by this worker.
+    CACHE_PREFIX: 'offline-cache:wfwalker/lectionaryclock:' + (self.registration ? self.registration.scope : '') + ':',
+
+    // This is the unique name for the cache controlled by this version of the worker.
+    get CACHE_NAME() {
+      return this.CACHE_PREFIX + '890d70aa9d5eb77c887d2290189a19b5135c9dd0';
+    },
+
+    // This is a list of resources that will be cached.
+    RESOURCES: [
+      './', // cache always the current root to make the default page available
+      './images/apple-touch-icon-114x114.png', // d18039f8fea99e40a95174ef9e9b1535bb31b19b
+      './images/apple-touch-icon-120x120.png', // 77fc407482f21d8a504b67a402ec24460e5cc178
+      './images/apple-touch-icon-144x144.png', // 088b71de4d4086c5f7e536beb1732b37d996a252
+      './images/apple-touch-icon-152x152.png', // 0da99f5359d8ed5d289f692f8df10b8472da4abc
+      './images/apple-touch-icon-57x57.png', // 05d01de8b368c72fddf4e7586d231ca0b43868af
+      './images/apple-touch-icon-60x60.png', // 6e19dde744cb62d4ec7b402ef5822a60f94390de
+      './images/apple-touch-icon-72x72.png', // 357e78f8ea62af3a9dc9869ac797871799277b16
+      './images/apple-touch-icon-76x76.png', // 8c81f23bd12150e31723405cb261aec8bb882961
+      './images/favicon-128x128.png', // c058f85521d02ca09dc4428fa27bc0750abb2e74
+      './images/favicon-16x16.png', // f159e70f0c1d2819d768e37b4999fb355d3bc2aa
+      './images/favicon-196x196.png', // 49f477e377bf66df6a5c939d4a8a016982dfb675
+      './images/favicon-32x32.png', // 07422f1e20069d66f825e794f2c6f54f0ee80d51
+      './images/favicon-96x96.png', // 0f621a46f7ce959046c1a781a8cabccdf36a6dd7
+      './images/mstile-144x144.png', // 1143726c60ee73b882af65e408745782282d618c
+      './images/mstile-150x150.png', // 70afb464b27eb37de2af5ed8c9e6857444eb0895
+      './images/mstile-310x150.png', // a8476b8ba5ea497c6acbc506ce416c616fe4f5da
+      './images/mstile-310x310.png', // 6cd3f41d4a135d5ec5695ade20845f40cefb34a4
+      './images/mstile-70x70.png', // 6f75379a90f9615b823d57537ffa8eb1a0887702
+      './images/next.png', // 942f3f721cc137fce5cc81cafa4e17119cc00100
+      './images/prev.png', // edf7f52c4498b1effb7694f3df8faed4fac37f81
+      './index.html', // 2bdcc28973180bcca4529d3eb1f613c45171acce
+      './scripts/app.js', // 2aff630504592cb2dad031d9228366bce05ee300
+      './scripts/d3.v3.min.js', // 751927cc0f71697c6b6f680192ac84a6d7ff3787
+      './scripts/main.js', // ebb75faf6f7e87caecc0e285253be2036df2ef1c
+      './scripts/offline-manager.js', // e2e09e000c5b64035940ae44e9c0936eb25ecd51
+      './styles/app.css', // e13e4cb2b3f3ab3699859803c27c4831368a2062
+      './styles/stylesheet.css', // 7997e49a29b051cadc22884144acdba1efdff492
+
+    ],
+
+    // Adds the resources to the cache controlled by this worker.
+    cacheResources: function () {
+      var now = Date.now();
+      var baseUrl = self.location;
+      return this.prepareCache()
+      .then(function (cache) {
+        return Promise.all(this.RESOURCES.map(function (resource) {
+          // Bust the request to get a fresh response
+          var url = new URL(resource, baseUrl);
+          var bustParameter = (url.search ? '&' : '') + '__bust=' + now;
+          var bustedUrl = new URL(url.toString());
+          bustedUrl.search += bustParameter;
+
+          // But cache the response for the original request
+          var requestConfig = { credentials: 'same-origin' };
+          var originalRequest = new Request(url.toString(), requestConfig);
+          var bustedRequest = new Request(bustedUrl.toString(), requestConfig);
+          return fetch(bustedRequest).then(function (response) {
+            if (response.ok) {
+              return cache.put(originalRequest, response);
+            }
+            console.error('Error fetching ' + url + ', status was ' + response.status);
+          });
+        }));
+      }.bind(this));
+    },
+
+    // Remove the offline caches not controlled by this worker.
+    clearOtherCaches: function () {
+      var deleteIfNotCurrent = function (cacheName) {
+        if (cacheName.indexOf(this.CACHE_PREFIX) !== 0 || cacheName === this.CACHE_NAME) {
+          return Promise.resolve();
+        }
+        return self.caches.delete(cacheName);
+      }.bind(this);
+
+      return self.caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(cacheNames.map(deleteIfNotCurrent));
+      });
+
+    },
+
+    // Get a response from the current offline cache or from the network.
+    get: function (request) {
+      return this.openCache()
+      .then(function (cache) {
+        return cache.match(request);
+      })
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return self.fetch(request);
+      });
+    },
+
+    // Prepare the cache for installation, deleting it before if it already exists.
+    prepareCache: function () {
+      return self.caches.delete(this.CACHE_NAME).then(this.openCache.bind(this));
+    },
+
+    // Open and cache the offline cache promise to improve the performance when
+    // serving from the offline-cache.
+    openCache: function () {
+      if (!this._cache) {
+        this._cache = self.caches.open(this.CACHE_NAME);
+      }
+      return this._cache;
+    }
+
+  };
+}(self));
---
+++ scripts/app.js
@@ -0,0 +1,295 @@
+// multiclock app.js
+
+// NOTA BENE: do this command
+// browserify app.js > bundle.js
+
+// dependencies
+var lectionary = require('lectionary');
+var dateformat = require('dateformat');
+
+var oneDay = 1000 * 60 * 60 * 24;
+
+var gClock = { currentSunday: null};
+
+// parse initial year from fragment
+var tmp = window.location.toString();
+if (tmp.indexOf('#') > 0) {
+	gClock.currentYear = parseInt(tmp.substring(tmp.indexOf('#')+1, tmp.indexOf('#')+5));
+} else {
+	gClock.currentYear = new Date().getFullYear();
+}
+
+function initializeSeasonCircle(face) {
+	var yearScale = d3.scale.linear().domain([gClock.beginYear.getTime(), gClock.endYear.getTime()]).range([0, 2 * Math.PI]);
+	
+	// set of arcs representing a year, showing liturgical calendar
+
+	var yearCircle = face.append('g')
+		.attr('id', 'yearCircle');
+
+	var seasons = lectionary.seasons(gClock.currentYear);
+
+	var seasonArc = d3.svg.arc()
+		.innerRadius(250)
+		.outerRadius(375)
+		.cornerRadius(10)
+		.startAngle(function(d) {
+			return yearScale(d.start.getTime());
+		})
+		.endAngle(function(d){ 
+			return yearScale(d.end.getTime());
+		});
+
+	yearCircle.selectAll("path")
+		.data(seasons)
+		.enter()
+		.append("path")
+		.attr("d", seasonArc)
+		.attr('stroke-width', 2)
+		.attr("class", function(d){
+			return 'season ' + d.name.replace(' ', '').toLowerCase();
+		})
+		.append('svg:title')
+		.text(function(d) { return d.name + ', ' + d.start.toLocaleDateString() + ' - ' + d.end.toLocaleDateString(); });
+
+	yearCircle.selectAll("path").data(seasons).enter();
+}
+
+function setBibleLink(id, passage) {
+	document.getElementById(id).href = 'http://biblegateway.com/passage/?version=NRSV&search=' + encodeURIComponent(passage);
+	document.getElementById(id).textContent = passage;
+}
+
+function clearSunday() {
+	document.getElementById('selectionName').textContent = '';
+	document.getElementById('dates').textContent = '';
+	document.getElementById('scriptureRadio').hidden = true;
+
+	document.getElementById('first').textContent = '';
+	document.getElementById('psalm').textContent = '';
+	document.getElementById('second').textContent = '';
+	document.getElementById('gospel').textContent = '';
+}
+
+// link to http://www.elca.org/Our-Work/Congregations-and-Synods/Worship/Lectionary/YearB
+
+function showSunday(aSunday) {
+	document.getElementById('selectionName').textContent = aSunday.lectionaryShortName + ', Year ' + aSunday.lectionaryYear;
+	document.getElementById('dates').textContent = dateformat(aSunday.date, "dddd, mmmm dS");
+
+	scriptures = aSunday.scriptures;
+
+	if (scriptures.complementary || scriptures.semicontinuous) {
+		document.getElementById('scriptureRadio').hidden = false;
+
+		if (scriptures.complementary && document.getElementById('scriptures_1').checked) {
+			scriptures = scriptures.complementary
+		} else if (scriptures.semicontinuous && document.getElementById('scriptures_2').checked) {
+			scriptures = scriptures.semicontinuous
+		}
+	} else {
+		document.getElementById('scriptureRadio').hidden = true;
+	}
+
+	setBibleLink('first', scriptures.first);
+	setBibleLink('psalm', scriptures.psalm);
+	setBibleLink('second', scriptures.second);
+	setBibleLink('gospel', scriptures.gospel);
+
+	d3.select('#pointer')
+		.attr('transform', function(d){
+			return 'rotate(' + gClock.yearDegreesScale(aSunday.date.getTime()) + ')';
+		});
+}
+
+function initializeWeekCircle(face) {
+	// set of arcs representing a year, showing weekly calendar
+
+	var weekCircle = face.append('g')
+		.attr('id', 'weekCircle');
+
+	var sundays = lectionary.days(gClock.currentYear);
+
+	// initialize currentSunday
+	for (var index in sundays) {
+		var aSunday = sundays[index];
+
+		var tmp = aSunday.scriptures;
+		if (tmp.complementary) {
+			tmp = tmp.complementary;
+		}
+
+		document.getElementById('csv').innerHTML += [
+			aSunday.date.toLocaleDateString(),
+			aSunday.lectionaryShortName,
+			tmp.first,
+			tmp.psalm,
+			tmp.second,
+			tmp.gospel,
+			'\n'
+		].join('>');
+
+		console.log(aSunday);
+
+		var delta = aSunday.date.getTime() - new Date().getTime();
+		if (delta > -1 * oneDay && delta < 6 * oneDay) {
+			gClock.currentSunday = aSunday;
+			showSunday(gClock.currentSunday);
+		}
+	}
+
+	// Add a text label for each Sunday
+	weekCircle.selectAll("text")
+		.data(sundays)
+		.enter()
+		.append("text")
+		.attr('class', 'dayLabel')
+		.attr("transform", function(d) {
+			var degrees = gClock.yearDegreesScale(d.date.getTime());
+
+			// orient the lefthand and righthand labels differently for legibility
+			if (degrees > 180) {
+				return 'rotate(' + (degrees + 90) + ') translate(-312,5)';
+			} else {
+				return 'rotate(' + (degrees + 270) + ') translate(312,5)';
+			}
+		})
+		.text(function (d) { return d.lectionaryShortName; })
+		.on('click', function(d) {
+			gClock.currentSunday = d;
+			showSunday(gClock.currentSunday);
+			d3.selectAll('.dayLabel').classed({selected: false});
+			this.classList.add('selected');
+		});
+}
+
+function initializeMonthCircle(face) {
+	var monthStarts = [];
+	for (var monthIndex = 0; monthIndex < 12; monthIndex++) {
+		var tmp = new Date(gClock.currentYear, monthIndex, 1);
+		monthStarts.push({ date: tmp, label: dateformat(tmp, "mmm") });
+	}
+
+	// START of DST
+    var firstOfMarch = new Date(gClock.currentYear, 2, 0); // zero-based month index
+    var daysUntilFirstSunday =  (7 - firstOfMarch.getDay()) % 7; //the day of week (0=Sunday, 6 = Saturday)
+ 	var secondSundayOfMarch = new Date(gClock.currentYear, 2, 7 + daysUntilFirstSunday);
+    monthStarts.push({ date: secondSundayOfMarch, label: 'DST' });
+
+    // END of DST
+    var firstOfNovember = new Date(gClock.currentYear, 10, 0); // zero-based month index
+    var daysUntilFirstSunday =  (7 - firstOfNovember.getDay()) % 7; //the day of week (0=Sunday, 6 = Saturday)
+ 	var firstSundayOfNovember = new Date(gClock.currentYear, 10, daysUntilFirstSunday);
+    monthStarts.push({ date: firstSundayOfNovember, label: 'DST' });
+
+	var monthCircle = face.append('g')
+		.attr('id', 'monthCircle');
+
+	// Add a text label for each Start of Month
+	monthCircle.selectAll("text")
+		.data(monthStarts)
+		.enter()
+		.append("text")
+		.attr('class', 'monthLabel')
+		.attr("transform", function(d) {
+			var degrees = gClock.yearDegreesScale(d.date.getTime());
+
+			// orient the lefthand and righthand labels differently for legibility
+			if (degrees > 180) {
+				return 'rotate(' + (degrees + 90) + ') translate(-235,5)';
+			} else {
+				return 'rotate(' + (degrees + 270) + ') translate(235,5)';
+			}
+		})
+		.text(function (d) { return d.label; });	
+}
+
+// INITIALIZE
+
+var vis = d3.select("#svg_donut");
+
+gClock.face = vis.append('g')
+	.attr('id','clock-face')
+	.attr('transform','translate(375,375)');	
+
+function showClockForYear(face, inNewYear) {
+	// remove everything
+	face.selectAll("*").remove();
+	clearSunday();
+
+	gClock.currentYear = inNewYear;
+	gClock.endYear = new Date(gClock.currentYear, 11, 31, 23, 59, 59);
+	gClock.beginYear = new Date(gClock.currentYear, 0, 1, 0, 0, 0);
+	gClock.yearDegreesScale = d3.scale.linear().domain([gClock.beginYear.getTime(), gClock.endYear.getTime()]).range([0, 360]);
+
+	initializeSeasonCircle(gClock.face);
+
+	// radial mark showing now
+
+	var pointerArc = d3.svg.arc()
+		.innerRadius(10)
+		.outerRadius(370)
+		.cornerRadius(3)
+		.startAngle(-0.04)
+		.endAngle(0.04);
+
+	gClock.face.append("path")
+		.attr('id', 'pointer')
+		.attr('stroke-width', 5)
+		.attr('d', pointerArc);
+
+	// circular overlay
+
+	var overlayArc = d3.svg.arc()
+		.innerRadius(0)
+		.outerRadius(150)
+		.cornerRadius(0)
+		.startAngle(0)
+		.endAngle(Math.PI * 2);
+
+	gClock.face.append("path")
+		.attr('id', 'circleOverlay')
+		.attr('stroke-width', 5)
+		.attr('d', overlayArc);
+
+	initializeWeekCircle(gClock.face);
+	initializeMonthCircle(gClock.face);
+
+	document.getElementById('timeView').textContent = gClock.currentYear;
+
+	document.getElementById('prevYear').setAttribute('data-year', gClock.currentYear - 1);
+	document.getElementById('nextYear').setAttribute('data-year', gClock.currentYear + 1);
+
+	d3.select('#pointer')
+		.attr('transform', function(d){
+			var tmp = new Date();
+			return 'rotate(' + gClock.yearDegreesScale(tmp.getTime()) + ')';
+		});
+}
+
+// INITIALIZE
+showClockForYear(gClock.face, gClock.currentYear);
+
+// wire up links to next year, previous year buttons
+var yearlinks = document.getElementsByClassName('yearlink');
+for (var index = 0; index < yearlinks.length; index++) {
+	var yearlink = yearlinks[index];
+
+	yearlink.addEventListener('click', function (e) {
+		e.preventDefault();
+		var tmp = parseInt(this.getAttribute('data-year'));
+		showClockForYear(gClock.face, tmp);
+		history.pushState(null, '', '#' + tmp);
+	});
+}
+
+// get events when complementary / semicontinuous changes
+document.getElementById('scriptures_1').addEventListener('change', function (e){
+	showSunday(gClock.currentSunday);
+});
+document.getElementById('scriptures_2').addEventListener('change', function (e){
+	showSunday(gClock.currentSunday);
+});
+
+
+
